10af8c8834485350622c06a5449b264e9c766b74
Kgatatsoo <vibrationbanda@gmail.com>
2025-09-14 21:38:02 +0200

    Delete python_introduction/finance_calculator.py

--- python_introduction/finance_calculator.py
+++
@@ -1,8 +0,0 @@
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your total monthly expenses: "))
-
-monthly_savings = income - expenses
-projected_savings = monthly_savings * 12 * (1 + 0.05)
-
-print(f"Your monthly savings are ${monthly_savings:.2f}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
